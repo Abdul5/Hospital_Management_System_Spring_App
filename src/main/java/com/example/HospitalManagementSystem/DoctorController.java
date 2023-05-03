d0079a99a1e5ebb59a14bd4f4800c0cd96baaea9
abdul5 <quadir5.muz@gmail.com>
2023-05-04 03:36:13 +0530

    Third Spring App

---
+++ src/main/java/com/example/HospitalManagementSystem/DoctorController.java
@@ -0,0 +1,24 @@
+package com.example.HospitalManagementSystem;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/doctor")
+public class DoctorController {
+
+    HashMap<Integer,Doctor> doctorDb = new HashMap<>();
+
+    @PostMapping("/add")
+    public String addDoctor(@RequestBody Doctor doctor){
+
+        int doctorId = doctor.getDoctorId();
+        doctorDb.put(doctorId,doctor);
+
+        return "doctor added successfully";
+    }
+
+
+
+}
